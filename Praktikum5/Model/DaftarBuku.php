051e2cfd1477918e660b0de85140d86ba7e5d723
4nnisaa <surahannisa961@gmail.com>
2024-10-08 11:31:29 +0700

    menambahkan tugas 5
    commit -m

---
+++ Praktikum5/Model/DaftarBuku.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "Buku.php";
+
+class DaftarBuku{
+
+    public function getData(){
+        $daftar_buku = array(
+            new Buku('Belajar Pemrograman Web', 'Surah Annisa', 'Informatika', '2024'),
+            new Buku('Matematika Diskrit', 'Taylor', 'Andi Publisher', '2020'),
+            new Buku('Kalkulus', 'Ariana', 'Erlangga', '2019'),
+            new Buku('Metodologi Penelitian', 'Adelle', 'Pustaka UIN', '2024'),
+        );
+
+        return $daftar_buku;
+    }
+
+    public function getKolomTabel(){
+        return array('No', 'Judul', 'Pengarang', 'Penerbit', 'Tahun');
+    }
+    
+}
